1cba14fb3730e9d75897f23c408c737df4759b8f
Henrique Cruz <rick_mv@hotmail.com>
2026-07-28 21:58:26 -0300

    Preparar CORS para produção

--- src/server.ts
+++ src/server.ts
@@ -88,7 +88,7 @@ const start = async () => {
   try {
 
     await app.register(cors, {
-      origin: "http://localhost:3000"
+      origin: process.env.FRONTEND_URL || "http://localhost:3000"
     });
 
 
